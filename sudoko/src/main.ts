d548b9e47a2ce27304633acf7549008f6cb828ec
Frank Müller <101019006+fmabap@users.noreply.github.com>
2023-03-13 08:01:38 +0100

    Test Conv

--- sudoko/src/main.ts
+++ sudoko/src/main.ts
@@ -2,16 +2,41 @@ import { GRID_SIZE } from "./declarations";
 import Generator from "./generator";
 import Ui from "./ui";
 
+testConv();
 const generator = new Generator();
 const ui = new Ui(generator);
 ui.requestCountNumbers(showBoard);
 
+function testConv() {
+    let bla: string = "";
+    let valueStr: string = "";
+    for (let idx = 0; idx < 82; idx++) {
+        let value = Math.floor(Math.random() * GRID_SIZE);
+        valueStr = valueStr + value.toString();
+        bla = bla + String.fromCodePoint(value + 65);
+    }
+
+    let base64: string = btoa(bla);
+    console.log(valueStr);
+    console.log(bla);
+    console.log(base64);
+    let res = atob(base64);
+    console.log(res);
+    let numStr: string = "";
+    let char: string;
+    for (let idx = 0; idx < 82; idx++) {
+        char = bla.substring(idx, idx + 1);
+        numStr = numStr + (char.charCodeAt(0) - 65).toString();
+        //    bla = bla + String.fromCharCode(value + 65);
+
+    }
+    console.log(numStr);
+
+}
 
 function showBoard() {
     generator.generateSudoko();
     generator.printBoard();
     generator.reduceBoard((GRID_SIZE * GRID_SIZE) - ui.getCountNumbers());
     ui.initBoard(generator.getBoard());
-    ui.placeOnBoard();
-
 }
