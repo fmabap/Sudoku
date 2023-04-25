b23d9c1e5752144da99da27be81c7fe277c9ae20
Frank Müller <101019006+fmabap@users.noreply.github.com>
2023-04-25 21:15:07 +0200

    Create PWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // vite.config.js
 export default defineConfig({
     // config options
+    plugins: [VitePWA(
+        {
+            manifest: {
+                name: "Sudoku",
+                short_name: "Sudoku",
+                theme_color: "#317EFB",
+                icons: [
+                    {
+                        src: "./icon.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any maskable"
+                    }
+                ]
+            }
+        })],
     base: "./",
     root: "./src",
     build: {
